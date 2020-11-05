ca609434b9c49d794aead592356af8dc41e71ea5
Alex <40070530+antaxiom@users.noreply.github.com>
2020-11-05 17:30:53 -0400

    Colorscheme revamp: wip, changes will come soon

--- autoload/onedark.vim
+++ autoload/onedark.vim
@@ -3,15 +3,15 @@
 let s:overrides = get(g:, "onedark_color_overrides", {})
 
 let s:colors = {
-      \ "red": get(s:overrides, "red", { "gui": "#FF5555", "cterm": "204", "cterm16": "1" }),
+      \ "red": get(s:overrides, "red", { "gui": "#F43D3D", "cterm": "204", "cterm16": "1" }),
       \ "dark_red": get(s:overrides, "dark_red", { "gui": "#f44747", "cterm": "196", "cterm16": "9" }),
-      \ "green": get(s:overrides, "green", { "gui": "#98C379", "cterm": "114", "cterm16": "2" }),
-      \ "yellow": get(s:overrides, "yellow", { "gui": "#E5C07B", "cterm": "180", "cterm16": "3" }),
+      \ "green": get(s:overrides, "green", { "gui": "#50fa7b", "cterm": "114", "cterm16": "2" }),
+      \ "yellow": get(s:overrides, "yellow", { "gui": "#E5C76B", "cterm": "180", "cterm16": "3" }),
       \ "dark_yellow": get(s:overrides, "dark_yellow", { "gui": "#D19A66", "cterm": "173", "cterm16": "11" }),
       \ "blue": get(s:overrides, "blue", { "gui": "#61AFEF", "cterm": "39", "cterm16": "4" }),
-      \ "purple": get(s:overrides, "purple", { "gui": "#C678DD", "cterm": "170", "cterm16": "5" }),
-      \ "cyan": get(s:overrides, "cyan", { "gui": "#56B6C2", "cterm": "38", "cterm16": "6" }),
-      \ "white": get(s:overrides, "white", { "gui": "#CCCCCC", "cterm": "145", "cterm16": "7" }),
+      \ "purple": get(s:overrides, "purple", { "gui": "#CF4DDD", "cterm": "170", "cterm16": "5" }),
+      \ "cyan": get(s:overrides, "cyan", { "gui": "#36C8ED", "cterm": "38", "cterm16": "6" }),
+      \ "white": get(s:overrides, "white", { "gui": "#DDDDDD", "cterm": "145", "cterm16": "7" }),
       \ "black": get(s:overrides, "black", { "gui": "#111111", "cterm": "235", "cterm16": "0" }),
       \ "visual_black": get(s:overrides, "visual_black", { "gui": "NONE", "cterm": "NONE", "cterm16": "0" }),
       \ "comment_grey": get(s:overrides, "comment_grey", { "gui": "#3F3F49", "cterm": "59", "cterm16": "15" }),
--- build/build.js
+++ build/build.js
@@ -6,22 +6,22 @@ const { readFileSync, writeFileSync } = require('fs')
 const { resolve } = require('path')
 
 const baseColors = Object.freeze({
-	red: { gui: '#FF5555', cterm: '204', cterm16: '1' }, // alternate cterm: 168
+	red: { gui: '#F43D3D', cterm: '204', cterm16: '1' }, // alternate cterm: 168
 	dark_red: { gui: '#f44747', cterm: '196', cterm16: '9' },
-	green: { gui: '#98C379', cterm: '114', cterm16: '2' },
-	yellow: { gui: '#E5C07B', cterm: '180', cterm16: '3' },
+	green: { gui: '#50fa7b', cterm: '114', cterm16: '2' },
+	yellow: { gui: '#E5C76B', cterm: '180', cterm16: '3' },
 	dark_yellow: { gui: '#D19A66', cterm: '173', cterm16: '11' },
 	blue: { gui: '#61AFEF', cterm: '39', cterm16: '4' }, // alternate cterm: 75
-	purple: { gui: '#C678DD', cterm: '170',  cterm16: '5' }, // alternate cterm: 176
-	cyan: { gui: '#56B6C2', cterm: '38', cterm16: '6' }, // alternate cterm: 73
-	white: { gui: '#CCCCCC', cterm: '145', cterm16 : '7' },
-	black: { gui: '#111111', cterm: '235', cterm16: '0' }
+	purple: { gui: '#CF4DDD', cterm: '170', cterm16: '5' }, // alternate cterm: 176
+	cyan: { gui: '#36C8ED', cterm: '38', cterm16: '6' }, // alternate cterm: 73
+	white: { gui: '#DDDDDD', cterm: '145', cterm16: '7' },
+	black: { gui: '#111111', cterm: '235', cterm16: '0' },
 })
 
 const specialColors = Object.freeze({
 	comment_grey: { gui: '#3F3F49', cterm: '59', cterm16: '15' },
 	gutter_fg_grey: { gui: '#45454F', cterm: '238', cterm16: '15' },
-	cursor_grey:  { gui: '#1C1C1C', cterm: '236', cterm16: '8' },
+	cursor_grey: { gui: '#1C1C1C', cterm: '236', cterm16: '8' },
 	visual_grey: { gui: '#242424', cterm: '237', cterm16: '15' },
 	menu_grey: { cterm16: '8' }, // vim theme handles gui/cterm values
 	special_grey: { gui: '#2B2B2B', cterm: '238', cterm16: '15' },
@@ -32,7 +32,7 @@ const colors = Object.assign({}, baseColors, specialColors)
 
 const templateMap = Object.freeze({
 	'templates/autoload.template.vim': '../autoload/onedark.vim',
-	'templates/One Dark.Xresources': '../term/One Dark.Xresources'
+	'templates/One Dark.Xresources': '../term/One Dark.Xresources',
 })
 
 const shouldCheck = String(process.argv[2]).toLowerCase() === 'check'
@@ -44,14 +44,12 @@ const handleError = (message, cause) => {
 }
 
 console.log(
-	shouldCheck ?
-		'Checking for inconsistencies between templates and existing output files...'
-		:
-		'Generating output files from templates...'
+	shouldCheck
+		? 'Checking for inconsistencies between templates and existing output files...'
+		: 'Generating output files from templates...'
 )
 
-Object.keys(templateMap).forEach(templateFilename => {
-
+Object.keys(templateMap).forEach((templateFilename) => {
 	// Read the template
 	const templatePath = resolve(__dirname, templateFilename)
 	let templateText
@@ -82,9 +80,13 @@ Object.keys(templateMap).forEach(templateFilename => {
 		const existingOutput = readFileSync(outputPath, 'utf8')
 		// Only need to do something if the generated output differs from what's already on disk
 		if (output !== existingOutput) {
-			if (shouldCheck) { // Check generated output against existing output file
-				handleError(`Changes were made to ${templateMap[templateFilename]} that are inconsistent with its template (${templateFilename}).\nDo you need to [re]build?`)
-			} else { // Overwrite existing output file
+			if (shouldCheck) {
+				// Check generated output against existing output file
+				handleError(
+					`Changes were made to ${templateMap[templateFilename]} that are inconsistent with its template (${templateFilename}).\nDo you need to [re]build?`
+				)
+			} else {
+				// Overwrite existing output file
 				try {
 					writeFileSync(outputPath, output, 'utf8')
 				} catch (e) {
@@ -95,12 +97,14 @@ Object.keys(templateMap).forEach(templateFilename => {
 	} catch (e) {
 		handleError(`Error reading existing output file ${outputPath}`, e)
 	}
-
 })
 
 try {
 	// Use the Xresources theme as a color source since it was generated above via templating
-	const xresources = readFileSync(resolve(__dirname, '../term/One Dark.Xresources'), 'utf8')
+	const xresources = readFileSync(
+		resolve(__dirname, '../term/One Dark.Xresources'),
+		'utf8'
+	)
 	const terminalPalette = termcolors.xresources.import(xresources)
 
 	let alacrittyTemplate, itermTemplate, kittyTemplate, terminalAppTemplate
@@ -131,8 +135,14 @@ try {
 
 		// From termcolors/lib/formats/terminal-app.js
 		const code = [
-			Buffer.from('62706c6973743030d40102030405061516582476657273696f6e58246f626a65637473592461726368697665725424746f7012000186a0a307080f55246e756c6cd3090a0b0c0d0e554e535247425c4e53436f6c6f7253706163655624636c6173734f1027', 'hex'),
-			Buffer.from('0010018002d2101112135a24636c6173736e616d655824636c6173736573574e53436f6c6f72a21214584e534f626a6563745f100f4e534b657965644172636869766572d1171854726f6f74800108111a232d32373b41484e5b628c8e9095a0a9b1b4bdcfd2d700000000000001010000000000000019000000000000000000000000000000d9', 'hex')
+			Buffer.from(
+				'62706c6973743030d40102030405061516582476657273696f6e58246f626a65637473592461726368697665725424746f7012000186a0a307080f55246e756c6cd3090a0b0c0d0e554e535247425c4e53436f6c6f7253706163655624636c6173734f1027',
+				'hex'
+			),
+			Buffer.from(
+				'0010018002d2101112135a24636c6173736e616d655824636c6173736573574e53436f6c6f72a21214584e534f626a6563745f100f4e534b657965644172636869766572d1171854726f6f74800108111a232d32373b41484e5b628c8e9095a0a9b1b4bdcfd2d700000000000001010000000000000019000000000000000000000000000000d9',
+				'hex'
+			),
 		]
 
 		terminalAppTemplate = termcolors.export(
@@ -140,31 +150,43 @@ try {
 			// From termcolors/lib/formats/terminal-app.js
 			_.partialRight(_.mapValues, function (color) {
 				var srgb = color.toAvgRgbArray()
-				srgb = srgb.map(function (n) {
-					return n.toFixed(10).toString()
-				}).join(' ')
-				var output = code[0].toString('binary') + srgb + code[1].toString('binary')
-				output = (Buffer.from(output, 'binary')).toString('base64')
+				srgb = srgb
+					.map(function (n) {
+						return n.toFixed(10).toString()
+					})
+					.join(' ')
+				var output =
+          code[0].toString('binary') + srgb + code[1].toString('binary')
+				output = Buffer.from(output, 'binary').toString('base64')
 				return output.match(/.{1,68}/g).join('\n\t')
 			})
 		)
-
 	} catch (e) {
 		handleError('Error compiling terminal color template', e)
 	}
 
 	try {
-		writeFileSync(resolve(__dirname, '../term/One Dark.alacritty'), alacrittyTemplate(terminalPalette))
-		writeFileSync(resolve(__dirname, '../term/One Dark.itermcolors'), itermTemplate(terminalPalette))
-		writeFileSync(resolve(__dirname, '../term/One Dark.kitty'), kittyTemplate(terminalPalette))
-		writeFileSync(resolve(__dirname, '../term/One Dark.terminal'), terminalAppTemplate(terminalPalette))
+		writeFileSync(
+			resolve(__dirname, '../term/One Dark.alacritty'),
+			alacrittyTemplate(terminalPalette)
+		)
+		writeFileSync(
+			resolve(__dirname, '../term/One Dark.itermcolors'),
+			itermTemplate(terminalPalette)
+		)
+		writeFileSync(
+			resolve(__dirname, '../term/One Dark.kitty'),
+			kittyTemplate(terminalPalette)
+		)
+		writeFileSync(
+			resolve(__dirname, '../term/One Dark.terminal'),
+			terminalAppTemplate(terminalPalette)
+		)
 	} catch (e) {
 		handleError('Error writing terminal color file', e)
 	}
-
 } catch (e) {
 	handleError('Error reading Xresources terminal color file', e)
 }
 
 console.log('Success!')
-
